737cbd5f4f7a33f5db108f854051897c6f9914f2
kei224022 <ishikawa224022@protonmail.com>
2023-09-12 09:40:13 +0900

    Initial Commit

---
+++ Football-data/Football_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_dataApp.swift
+//  Football-data
+//
+//  Created by member on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct Football_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
